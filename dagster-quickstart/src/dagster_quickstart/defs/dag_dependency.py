d9764b18f1472b760bb2de8f4bdc94d329473778
Kurinchiban <kurinchiban003@gmail.com>
2025-10-06 15:37:27 +0530

    Change in dagster file    stucture

--- dagster-quickstart/src/dagster_quickstart/defs/dag_dependency.py
+++ dagster-quickstart/src/dagster_quickstart/defs/dag_dependency.py
@@ -41,3 +41,5 @@ defs = dg.Definitions(
         )
     ]
 )
+
+
--- dagster-quickstart/src/dagster_quickstart/defs/process_csv_file.py
+++ dagster-quickstart/src/dagster_quickstart/defs/process_csv_file.py
@@ -6,7 +6,7 @@ processed_data_file = "src/dagster_quickstart/defs/data/processed_data.csv"
 
 
 @dg.asset
-def processed_data():
+def processed_data(context: dg.AssetExecutionContext):
     ## Read data from the CSV
     df = pd.read_csv(sample_data_file)
 
@@ -17,4 +17,14 @@ def processed_data():
 
     ## Save processed data
     df.to_csv(processed_data_file, index=False)
+    
+    # Emit metadata to Dagit UI
+    context.add_output_metadata(
+        {
+            "row_count": len(df),
+            "preview": dg.MetadataValue.md(df.head().to_markdown()),  # shows sample rows
+            "file_path": processed_data_file,
+        }
+    )
+    
     return "Data loaded successfully"
